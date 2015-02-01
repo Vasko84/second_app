dd348e92bd22d22467ba3cfbba92f9d5f7596b08
Vasko84 <Vasilij.test84@gmail.com>
2015-02-01 15:21:17 +0000

    adding title function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    common_title="Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      then 
+        common_title
+      else
+        "#{common_title} | #{page_title}"
+    end
+  end
+  
+  
+  
 end
