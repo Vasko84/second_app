628f83f470d4239fbde5d130f6823d1b8abd2246
Vasko84 <Vasilij.test84@gmail.com>
2015-02-05 20:16:50 +0000

    creating User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe User do
+  before {@user=User.new(name:"Vas", email:"vas@vas.va")}
+  subject { @user}
+  
+  it {should respond_to(:name)}
+  it {should respond_to(:email)}
+end
