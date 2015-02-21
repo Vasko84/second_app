192ea8a774ca83b5633de793e7d19e0a63136d28
Vasko84 <Vasilij.test84@gmail.com>
2015-02-21 13:27:35 +0000

    adding signup form (with css), tests for it. Should dig in css. Next 7.2.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,11 +5,33 @@ describe "User Pages" do
   subject {page}
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   
-  describe "GET /user_pages" do
+  describe "Sign Up page" do
     before {visit signup_path}
+    let(:submit) {"Create my account"}
     
-    it {should have_content('SignUp')}
-    it {should have_title("#{base_title} | SignUp")}
+    describe "content and title" do
+      it {should have_content('SignUp')}
+      it {should have_title("#{base_title} | SignUp")}
+    end
+    
+    describe "with invalid information" do
+      it "should not create user" do
+        expect {click_button submit}.not_to change(User, :count)
+      end
+    end
+   
+    describe "with valid information" do
+         before do
+          fill_in "Name", with: "Example_user"
+          fill_in "Email", with: "example@example.com"
+          fill_in "Password", with: "password"
+          fill_in "Confirmation", with: "password"
+        end     
+      it "should create a user " do
+        expect {click_button submit}.to change(User, :count).by(1)
+      end
+    end
+    
   end
 
   describe "Profile pages" do
