d4dad2e8a732b11507fea75ade1a31fbc2265812
Vasko84 <Vasilij.test84@gmail.com>
2015-02-22 12:42:53 +0000

    adding tests for registration. End of ch.7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,15 +23,53 @@ describe "User Pages" do
     describe "with valid information" do
          before do
           fill_in "Name", with: "Example_user"
-          fill_in "Email", with: "example@example.com"
+           fill_in "Email", with: "user@example.com"
           fill_in "Password", with: "password"
           fill_in "Confirmation", with: "password"
         end     
       it "should create a user " do
         expect {click_button submit}.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
     
+    describe "errors in registration" do
+      describe "blank field" do
+      before {click_button submit}
+      it {should have_content("error")}
+      it {should have_content("Name can't be blank")}
+      it {should have_content("Email can't be blank")}
+      it {should have_content("Password can't be blank")}
+      end
+      
+      describe "short/invalid fields" do
+        before do
+          fill_in "Name", with:"A"
+          fill_in "Email", with:"B"
+          fill_in "Password", with:"123"
+          fill_in "Confirmation", with:"124"
+          click_button submit
+        end
+        it {should have_content("Name is too short")}
+        it {should have_content("Email is invalid")}
+        it {should have_content("Password is too short")}
+        it {should have_content("Password confirmation doesn't match Password")}
+      end
+      
+      describe "long name" do
+        before do
+          fill_in "Name", with:"a"*51
+          click_button submit
+        end
+        it {should have_content("Name is too long")}
+      end
+    end
   end
 
   describe "Profile pages" do
