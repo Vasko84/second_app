87c762fcc6f01c313fda8df926b0d1c51b5b9fbc
Vasko84 <Vasilij.test84@gmail.com>
2015-02-28 07:40:32 +0000

    defining sign_in method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
   def sign_in
+    remember_token=User.new_remember_token
+    cookies.permanent[:remember_token]=remember.token
+    user.update_attributes(:remember_token, User.encrypt(remember_token))
+    self.current_user=user      
+  end
+    
+  def current_user=(user)
+    @current_user=user
   end
   
 end
